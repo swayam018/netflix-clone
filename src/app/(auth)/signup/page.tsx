0d2499f7e1f10a223496e2b9099b89ed277473fc
Swayam Sarthak Swain <swayamsarthakswain018@gmail.com>
2023-12-10 01:22:27 +0530

    padding added

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -12,9 +12,9 @@ function Signup() {
         <div className='bg-white'>
             <Navbar opacity={0} slate={50} />
             <hr />
-            <div className=' bg-slate-50 h-full flex flex-col items-center gap-10 pt-10'>
-                <div className=' [&>*]:text-slate-950 flex flex-col gap-5 w-82 justify-center max-[458px]:w-76'>
-                    <Image src={tickround} alt='settingup' width={40} height={40} className=' m-auto'/>
+            <div className=' bg-slate-50 h-full flex flex-col items-center gap-10 pt-10 '>
+                <div className=' [&>*]:text-slate-950 flex flex-col gap-5 w-82 justify-center max-[458px]:w-76 pb-16'>
+                    <Image src={tickround} alt='settingup' width={40} height={40} className=' m-auto' />
                     <span className='text-center pt-4'>STEP 2 OF 3</span>
                     <span className=' text-4xl font-bold text-center'>Choose your plan.</span>
                     <div className=' flex flex-row gap-4 items-center justify-start'>
@@ -30,8 +30,8 @@ function Signup() {
                         <span className='text-lg'>No ads and no extra fees. Ever.</span>
                     </div>
                     <button className=' bg-red-700 px-8 py-2 rounded-md w-full hover:bg-customred-400 mt-4'><Link href="/signup/signupform" className=' text-slate-50 text-2xl'>Next</Link></button>
-                    <Footer />
                 </div>
+                <Footer />
             </div>
         </div>
     )
